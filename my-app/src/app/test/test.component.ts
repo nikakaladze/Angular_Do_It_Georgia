46a1440579393acc1372868aaafbb865893a828e
nikakaladze <nikakaladze89@gmail.com>
2025-02-27 11:30:00 +0400

    chore:create test component

---
+++ my-app/src/app/test/test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  imports: [],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.css'
+})
+export class TestComponent {
+
+  fmessage:string="hello"
+}
