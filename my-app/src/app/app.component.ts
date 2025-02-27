98b48af2edf2660f6d20b6ed6bfc2d5d78b35eab
nikakaladze <nikakaladze89@gmail.com>
2025-02-27 19:52:26 +0400

    chore:restore some conflicts

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,25 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 import { TestComponent } from "./test/test.component";
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, TestComponent],
-=======
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
->>>>>>> 145389a594d5fc18450c90fb015da3ca8cf40d2e
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-<<<<<<< HEAD
   message:string = 'hello world from Angular';
 
-=======
-  title = 'my-app';
->>>>>>> 145389a594d5fc18450c90fb015da3ca8cf40d2e
 }
