3103c2e8a871374bcbfab80a1ed737510ca7612a
nikakaladze <nikakaladze89@gmail.com>
2025-03-10 22:49:43 +0400

    chore:create Custom Directives for Some Practice

--- my-app/src/app/user-profile/user-profile.component.ts
+++ my-app/src/app/user-profile/user-profile.component.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
 import {HighlightDirective} from "../directives/highlight.directive";
+import {NgForOf, NgIf, NgSwitch, NgSwitchCase} from '@angular/common';
+import {RepeatDirective} from '../directives/repeat.directive';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [HighlightDirective],
+  imports: [HighlightDirective, NgIf, NgForOf, NgSwitch, NgSwitchCase, RepeatDirective],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
+isLoggedIn: boolean = false;
+useRole :string = "IsSecurity";
 
-  protected readonly HighlightDirective = HighlightDirective;
+
+players  = [
+  {
+    "id": 26,
+    "firstName": "John",
+    "lastName": "Terry",
+    "position": "Defender",
+  },
+  {
+    "id": 12,
+    "firstName": "John",
+    "lastName": "Obi Mikel",
+    "position": "CMD",
+  },
+  {
+    "id": 8,
+    "firstName": "Frank",
+    "lastName": "Lampard",
+    "position": "CM",
+  },
+  {
+    "id": 11,
+    "firstName": "Didier",
+    "lastName": "Drogba",
+    "position": "CenterForward",
+
+  }
+]
 }
